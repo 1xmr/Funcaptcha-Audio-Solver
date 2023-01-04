b14f6f52bc15215652fbace07194e91c0c6b9d34
1xmr <109765426+1xmr@users.noreply.github.com>
2023-01-04 13:15:00 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+os.system("pip install -r requirements.txt")
+from funcaptcha_solver import funcaptcha
+import aio5
+
+#Replace public_key and site with your key/site
+funcap = funcaptcha(
+	public_key = "", 
+	site = ""
+)
+
+bad_captchas = 0
+
+def solve_captcha():
+	global bad_captchas
+	while True:
+		answer = funcap.solve()
+		captcha_token = answer["token"]
+		error = answer["error"]
+		if error == None:
+			answer["bad_captchas"] = bad_captchas
+			return captcha_token
+		if "not_supported" in error:
+			return "Site not supported / does not have audio challenges"
+		if "too high" in error:
+			return "Ratelimited"
+		else:
+			bad_captchas += 1
+
+captcha_token = solve_captcha()
+print(captcha_token)
